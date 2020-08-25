900e43af6e929e58d2de8e7ddce9cf3d70b84c4a
Mathieu Muller <mathieu.muller@uppler.com>
2020-08-25 23:15:46 +0200

    jj

--- index.js
+++ index.js
@@ -36,8 +36,12 @@ async function requestReviews() {
 		requestedReviewers = [core.getInput('permanent_reviewer')],
 		countReviewers = 1;
 
+		console.log(collaborators);
 	collaborators.forEach(function(collaborator) {
 		let login = collaborator.login;
+		console.log(login);
+		console.log(requestedReviewers.length);
+		console.log(core.getInput('reviewers_number'));
 
 		if (requestedReviewers.length < core.getInput('reviewers_number') && !requestedReviewers.includes(login)) {
 			// add reviewer
