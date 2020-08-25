1d23c936fc6416be548a796b5eec3ee1db5dddf9
Mathieu Muller <mathieu.muller@uppler.com>
2020-08-25 23:51:08 +0200

    p

--- index.js
+++ index.js
@@ -29,18 +29,17 @@ try {
  * Requests reviewers on the pull request
  */
 async function requestReviews() {
-	let { data: currentReviewers } = await octokit.pulls.listRequestedReviewers({
-	  owner: repository_owner,
-	  repo: repository_name,
-	  pull_number: context.number,
-	}),
-		requestedReviewers = []
-	;
+	// let { data: currentReviewers } = await octokit.pulls.listRequestedReviewers({
+	//   owner: repository_owner,
+	//   repo: repository_name,
+	//   pull_number: context.number,
+	// }),
+	let requestedReviewers = [];
 
 	// keep reviewers if they were already assigned
-	currentReviewers.forEach(function(reviewer) {
-		requestReviewers.push(reviewer.login);
-	});
+	// currentReviewers.forEach(function(reviewer) {
+	// 	requestReviewers.push(reviewer.login);
+	// });
 
 	// always add the permanent reviewer
 	if (!requestedReviewers.includes(core.getInput('permanent_reviewer'))) {
