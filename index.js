322cb1a3db9f3ddb6ffc6f4d027592c2227888d3
Mathieu Muller <mathieu.muller@uppler.com>
2020-08-26 17:35:58 +0200

    v1

--- index.js
+++ index.js
@@ -1,25 +1,103 @@
+/**
+ * PULL REQUEST WORKFLOW ACTION
+ *
+ * 1. The developer is done, he adds the status RFT to its pull request -> the pull request is locked (mergeable false) -> acceptance tests are triggered
+ * 2. a. Tests fails -> add label FFF -> assign author , the developer fixes its PR and add the label RFT when he's done
+ *    b. Tests success -> add label RFR -> unassign author + request and assign reviewers (permanent + 2 randoms)
+ * 3. a. Review changes_requested -> unassign reviewer -> remove label RFR -> add label FFF -> developer fixes its PR and add the label RFT to reenter tests process after his fixes
+ *    b. Review comments -> assign author -> nothing happens the developer answer the comment and the reviewer decides if he approves or refuse the PR
+ *    c. Review approved -> unassign reviewer
+ *    					 -> if 2 approvals and label is RFR -> remove label RFR -> add label RTM -> unassign author -> assign mergeator -> unlock PR
+ */
+
 const core = require('@actions/core'),
-	token=core.getInput('token'),
+	token = core.getInput('token'),
 	github = require('@actions/github'),
 	context = github.context,
+	octokit = github.getOctokit(token),
 	eventName = context.eventName,
 	payload = context.payload,
-	action=payload.action,
-	octokit = github.getOctokit(token),
+	action = payload.action,
 	repository = process.env.GITHUB_REPOSITORY,
 	repository_owner = repository.split('/')[0],
-	repository_name = repository.split('/')[1]
+	repository_name = repository.split('/')[1],
+	pullRequest = payload.pull_request,
+	author = pullRequest.user.login,
+	pullNumber = pullRequest.number,
+	labelTest = core.getInput('label_test'),
+	labelReview = core.getInput('label_review'),
+	labelChangesRequested = core.getInput('label_changes_requested'),
+	labelApproved = core.getInput('label_approved'),
+	permanentReviewer = core.getInput('permanent_reviewer'),
+	mergeator = core.getInput('mergeator'),
+	reviewersNumber = core.getInput('reviewers_number'),
+	approvalsNumber = core.getInput('approvals_number')
 ;
 
 try {
 	if ('pull_request' === eventName) {
 		if ('labeled' === action) {
-			requestReviews();
+			let label = payload.label;
+
+			// add RFT label -> lock the PR (mergeable = false) and trigger acceptance tests,
+			if (label.name === labelTest) {
+				// TO DO
+			}
+
+			// add RFR label -> request and assign reviewers,
+			if (label.name === labelReview) {
+				requestReviews();
+			}
+
+			// add FFF label -> assign author,
+			if (label.name === labelChangesRequested) {
+				addAssignees([author]);
+			}
+
+			// add RTM label -> unassign author + assigne mergeator
+			if (label.name === labelChangesRequested) {
+				removeAssignees([author]);
+				addAssignees([mergeator]);
+			}
 		}
 	}
 
 	if ('pull_request_review' === eventName) {
-		console.log(context);
+		let review = payload.review
+			reviewer = review.user.login
+		;
+
+		if ('submitted' === action) {
+			// on changes requested -> remove RFR -> add FFF -> unassign reviewer
+			if ('changes_requested' === review.state) {
+				removeLabel(labelReview);
+				addLabel(labelChangesRequested);
+				removeAssignees([reviewer]);
+			}
+
+			// on comment -> assign author
+			if ('commented' === review.state) {
+				addAssignees([author]);
+			}
+
+			// on approve -> unassign reviewer
+			if ('approved' === review.state) {
+				removeAssignees([reviewer]);
+				// check number of approvals
+				if (isApproved()) {
+					// remove RFR and FFF and add RTM labels
+					addLabel(labelApproved);
+					removeLabel(labelReview);
+					removeLabel(labelChangesRequested);
+
+					// unassign reviewers + author and assign mergeator
+					unassignReviewers();
+					removeAssignees([author]);
+					addAssignees([mergeator]);
+				}
+			}
+		}
+
 	}
 } catch (error) {
   core.setFailed(error.message);
@@ -29,35 +107,41 @@ try {
  * Requests reviewers on the pull request
  */
 async function requestReviews() {
-	let pullRequest = payload.pull_request,
-		pullNumber = pullRequest.number,
-		pullAuthor = pullRequest.user.login,
-	    reviewers = await getReviewersList(pullNumber, pullAuthor)
-	;
+	let reviewers = await getReviewersList(pullNumber, pullAuthor);
 
 	// add the reviewers
-	octokit.pulls.requestReviewers({
-	  owner: repository_owner,
-	  repo: repository_name,
-	  pull_number: pullNumber,
-	  reviewers: reviewers
-	});
+	requestReviewers(reviewers);
 
 	// unassign the author
-	octokit.issues.removeAssignees({
-	  owner: repository_owner,
-	  repo: repository_name,
-	  issue_number: pullNumber,
-	  assignees: [pullAuthor]
-	});
+	removeAssignees([payload.pull_request.user.login]);
 
 	// assign the reviewers
-	octokit.issues.addAssignees({
-	  owner: repository_owner,
-	  repo: repository_name,
-	  issue_number: pullNumber,
-	  assignees: reviewers
+	addAssignees(reviewers);
+}
+
+/**
+ * Check that the pull request has at least 2 approvals and the permanent reviewer approval
+ */
+async function isApproved() {
+	let countApprovals = 0,
+		hasPermanentReviewerApproval = false,
+		{ data: reviews } = await octokit.pulls.listReviews({
+		  owner: repository_owner,
+		  repo: repository_name,
+		  pull_number: pullNumber,
+		})
+	;
+
+	reviews.forEach(function(review) {
+		if ('approved' === review.state) {
+			if (review.user.login === permanentReviewer) {
+				hasPermanentReviewerApproval = true;
+			}
+			++countApprovals;
+		}
 	});
+
+	return hasPermanentReviewerApproval && countApprovals >= approvalsNumber;
 }
 
 /**
@@ -65,11 +149,7 @@ async function requestReviews() {
  */
 async function getReviewersList(pullNumber, pullAuthor)
 {
-	let { data: currentReviewers } = await octokit.pulls.listRequestedReviewers({
-		  owner: repository_owner,
-		  repo: repository_name,
-		  pull_number: pullNumber,
-		}),
+	let currentReviewers = await listReviewers(),
 	    requestedReviewers = []
 	;
 
@@ -120,4 +200,80 @@ function shuffle(array) {
     }
 
     return array;
+}
+
+function addAssignees(assignees)
+{
+	octokit.issues.addAssignees({
+	  owner: repository_owner,
+	  repo: repository_name,
+	  issue_number: pullNumber,
+	  assignees: assignees
+	});
+}
+
+function removeAssignees(assignees)
+{
+	octokit.issues.removeAssignees({
+	  owner: repository_owner,
+	  repo: repository_name,
+	  issue_number: pullNumber,
+	  assignees: assignees
+	});
+}
+
+function requestReviewers(reviewers)
+{
+	octokit.pulls.requestReviewers({
+	  owner: repository_owner,
+	  repo: repository_name,
+	  pull_number: pullNumber,
+	  reviewers: reviewers
+	});
+}
+
+function removeLabel(label)
+{
+	octokit.issues.removeLabel({
+	  owner: repository_owner,
+	  repo: repository_name,
+	  issue_number: pullNumber,
+	  name: label
+	});
+}
+
+function addLabel(label)
+{
+	octokit.issues.addLabel({
+	  owner: repository_owner,
+	  repo: repository_name,
+	  issue_number: pullNumber,
+	  name: label
+	});
+}
+
+async function listReviewers()
+{
+	let { data: currentReviewers } = await octokit.pulls.listRequestedReviewers({
+	  owner: repository_owner,
+	  repo: repository_name,
+	  pull_number: pullNumber,
+	});
+
+	return currentReviewers,
+}
+
+/**
+ * Unassign the reviewers of the pull request
+ */
+async function unassignReviewers()
+{
+	let currentReviewers = await listReviewers(),
+		reviewers = []
+	;
+	currentReviewers.forEach(function (reviewer) {
+		reviewers.push(reviewer.login);
+	});
+
+	removeAssignees(reviewers);
 }
